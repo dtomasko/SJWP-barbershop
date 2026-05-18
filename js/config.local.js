63d31b8b62b4150436c19b8d49ef415623f97a24
dtomasko <dominik.tomasko.09@gmail.com>
2026-05-18 15:18:04 +0200

    added supabase config file

---
+++ js/config.local.js
@@ -0,0 +1,7 @@
+/**
+ * Kopiraj ovu datoteku kao js/config.local.js i zamijeni vrijednosti.
+ * js/config.local.js je u .gitignore.
+ * Na Vercelu build generira config.local.js iz environment varijabli.
+ */
+export const supabaseUrl = 'https://khmqjbiantcybamkewds.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtobXFqYmlhbnRjeWJhbWtld2RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NDQ4NjEsImV4cCI6MjA5NDMyMDg2MX0.dTnoV_nEnxhWxtps5MIr4HEavCVFCksIC4VTXRKZKfw';
